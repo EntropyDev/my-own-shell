456b553362ddbeb7c3ac8d81e79de593ee5ddafb
Vaibhav Chimalgi <vchimu20@gmail.com>
2025-11-23 10:39:13 +0530

    validate command

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 import sys
 
+def validate(cmd):
+    return False
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
-    pass
+    cmd = sys.stdin.readline().strip()
+    if not validate(cmd):
+        sys.stdout.write(f"{cmd}: command not found")    
 
 
 if __name__ == "__main__":
