95b87e6b5da2b4f1c632ef2784e2a6bc0416230d
Vaibhav Chimalgi <vchimu20@gmail.com>
2025-11-24 17:31:53 +0530

    type command

--- app/main.py
+++ app/main.py
@@ -1,29 +1,59 @@
 import sys
 
-def echo(*args):
-    s = " ".join([word for word in args])+"\n"
-    sys.stdout.write(s)
+class ExitException(Exception):
+    pass
 
-def validate(st):
-    st_a = st.split(" ")
-    [cmd, *args] = st_a
-    cmds = {
-        "echo": echo
-    }
-    if cmd in cmds:
-        cmds[cmd](*args)
-        return True
-    return False
+def cmd_register(cmds: dict, cmd: str, func_name: str):
+    def decorator(func):
+        cmds[cmd] = func.__name__
+        return func
+    return decorator
+
+class Terminal:
+    cmds = {}
+
+    @cmd_register(cmds, "exit")
+    @staticmethod
+    def _exit():
+        raise ExitException("Exiting")
+    
+    @cmd_register(cmds, "echo")
+    @staticmethod
+    def echo(cmd, *args):
+        s = " ".join([word for word in args])+"\n"
+        sys.stdout.write(s)
+    
+    @staticmethod
+    def cmd_not_found(cmd):
+        sys.stdout.write(f"{cmd}: command not found\n")
+
+    @cmd_register(cmds, "type")
+    @staticmethod
+    def typeof(cmd, *args):
+        type_cmd = " ".join(args)
+        if type_cmd in Terminal.cmds:
+            sys.stdout.write(f"{type_cmd} is a shell builtin\n")
+        else:
+            sys.stdout.write(f"{type_cmd}: not found\n")
+    @staticmethod
+    def execute(st):
+        st_a = st.split(" ")
+        [cmd, *args] = st_a
+        
+        if cmd in Terminal.cmds:
+            Terminal.cmds[cmd](cmd, *args)
+        else:
+            Terminal.cmd_not_found(cmd)
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
     while True:
         sys.stdout.write("$ ")
         cmd = input()
-        if cmd == "exit":
-            return 
-        if not validate(cmd):
-            sys.stdout.write(f"{cmd}: command not found\n")    
+        try:
+            Terminal().execute(cmd)
+        except ExitException as e:
+            return    
 
 
 if __name__ == "__main__":
