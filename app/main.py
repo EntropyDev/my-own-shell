a2b73364208454a0df9bcd419e06107286fc50f5
Vaibhav Chimalgi <vchimu20@gmail.com>
2025-11-24 18:38:01 +0530

    search exec in path dir

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 class ExitException(Exception):
     pass
@@ -10,48 +11,65 @@ def cmd_register(cmds: dict, cmd: str):
     return decorator
 
 class Terminal:
+
     cmds = {}
 
-    @cmd_register(cmds, "exit")
-    @staticmethod
-    def _exit(cmd, *args):
-        raise ExitException("Exiting")
-    
-    @cmd_register(cmds, "echo")
-    @staticmethod
-    def echo(cmd, *args):
-        s = " ".join([word for word in args])+"\n"
-        sys.stdout.write(s)
-    
     @staticmethod
     def cmd_not_found(cmd):
         sys.stdout.write(f"{cmd}: command not found\n")
 
+    @cmd_register(cmds, "exit")
+    @staticmethod
+    def _exit(*args, **kwargs):
+        raise ExitException("Exiting")
+    
+    @cmd_register(cmds, "echo")
+    @staticmethod
+    def echo(*args, **kwargs):
+        s = " ".join([word for word in args])+"\n"
+        sys.stdout.write(s)
+
     @cmd_register(cmds, "type")
     @staticmethod
-    def typeof(cmd, *args):
+    def typeof(*args, **kwargs):
         type_cmd = " ".join(args)
         if type_cmd in Terminal.cmds:
             sys.stdout.write(f"{type_cmd} is a shell builtin\n")
         else:
+            dirs = kwargs["PATH"].split(":")
+            for dir in dirs:
+                try:
+                    for file in os.listdir(dir):
+                        if file == type_cmd:
+                            file_path = dir+file
+                            if os.path.isfile(file_path) and os.access(file_path, os.X_OK):
+                                sys.stdout.write(f"{type_cmd} is {file_path}\n")
+                                return
+                except FileNotFoundError:
+                    pass
+        
             sys.stdout.write(f"{type_cmd}: not found\n")
+
     @staticmethod
-    def execute(st):
+    def execute(st, PATH):
         st_a = st.split(" ")
         [cmd, *args] = st_a
-        
+        kwargs = {
+            "PATH": PATH
+        }
         if cmd in Terminal.cmds:
-            Terminal.cmds[cmd](cmd, *args)
+            Terminal.cmds[cmd](*args,**kwargs)
         else:
             Terminal.cmd_not_found(cmd)
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
     while True:
+        PATH = os.environ["PATH"]
         sys.stdout.write("$ ")
         cmd = input()
         try:
-            Terminal.execute(cmd)
+            Terminal.execute(cmd, PATH)
         except ExitException as e:
             return    
 
